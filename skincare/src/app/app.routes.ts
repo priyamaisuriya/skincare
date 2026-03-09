587b0837b6b3c6b7e26c85b2768a678de1167183
unknown <drashti474@gmail.com>
2026-03-09 10:22:46 +0530

    user and couriers

--- skincare/src/app/app.routes.ts
+++ skincare/src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { Sidebar } from './component/admin/layout/sidebar/sidebar';
 import { Header } from './component/admin/layout/header/header';
 import { Layout } from './component/admin/layout/layout';
-import { List } from './component/admin/Sliders/list/list';
-import { Edit } from './component/admin/Sliders/edit/edit';
+import { List as CourierList } from './component/admin/Couriers/list/list';
+import { Edit as CourierEdit } from './component/admin/Couriers/edit/edit';
+import { List as UserList } from './component/admin/Users/list/list';
+import { Edit as UserEdit } from './component/admin/Users/edit/edit';
+
 
 export const routes: Routes = [
     {
@@ -12,9 +15,20 @@ export const routes: Routes = [
         children: [
             { path: 'sidebar', component: Sidebar },
             { path: 'header', component: Header },
-            { path: 'admin/slider', component: List },
-            { path: 'admin/slider/create', component: Edit },
-            { path: 'admin/slider/edit/:id', component: Edit }
+
+            // { path: 'admin/slider', component: List },
+            // { path: 'admin/slider/create', component: Edit },
+            // { path: 'admin/slider/edit/:id', component: Edit },
+
+            { path: 'admin/courier', component: CourierList },
+            { path: 'admin/courier/create', component: CourierEdit },
+            { path: 'admin/courier/edit/:id', component: CourierEdit },
+
+            { path: 'admin/user', component: UserList },
+            { path: 'admin/user/create', component: UserEdit },
+            { path: 'admin/user/edit/:id', component: UserEdit },
+
+
         ]
     }
 ];
---
+++ skincare/src/app/component/admin/Couriers/list/list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { Header as appHeader } from '../../layout/header/header';
+import { Sidebar as appSidebar } from '../../layout/sidebar/sidebar';
+
+@Component({
+  selector: 'app-list',
+  imports: [appHeader, appSidebar],
+  templateUrl: './list.html',
+  styleUrl: './list.css',
+})
+export class List { }
