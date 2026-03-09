50eb8e2992c6f81feb3b7caff52d316c09ae5c36
unknown <drashti474@gmail.com>
2026-03-09 12:30:36 +0530

    faq api

--- skincare/src/app/component/admin/FAQ/list/list.ts
+++ skincare/src/app/component/admin/FAQ/list/list.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Header as AppHeader } from '../../layout/header/header';
-import { Sidebar as AppSidebar } from '../../layout/sidebar/sidebar';
-
+import { FaqService } from '../../../../service/faq';
+import { Faq } from '../../../../models/faq';
 @Component({
   selector: 'app-list',
-  imports: [AppHeader, AppSidebar, RouterLink, FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
-export class List { }
+export class List implements OnInit {
+  faqs: Faq[] = [];
+
+  constructor(private faqService: FaqService) { }
+
+  ngOnInit(): void {
+    this.loadFaqs();
+  }
+
+  loadFaqs(): void {
+    this.faqService.getAllFaqs().subscribe({
+      next: (data) => {
+        this.faqs = data;
+      },
+      error: (err) => {
+        console.error('Error loading FAQs:', err);
+      }
+    });
+  }
+
+  deleteFaq(id: number): void {
+    if (confirm('Are you sure you want to delete this FAQ?')) {
+      this.faqService.deleteFaq(id).subscribe({
+        next: () => {
+          this.loadFaqs();
+        },
+        error: (err) => {
+          console.error('Error deleting FAQ:', err);
+        }
+      });
+    }
+  }
+}
---
+++ skincare/src/app/service/faq.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Faq } from '../models/faq';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FaqService {
+  private apiUrl = 'http://127.0.0.1:8000/api/admin/faq';
+
+  constructor(private http: HttpClient) { }
+
+  getAllFaqs(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  getFaqById(id: number): Observable<Faq> {
+    return this.http.get<Faq>(`${this.apiUrl}/${id}`);
+  }
+
+  createFaq(faq: Faq): Observable<Faq> {
+    return this.http.post<Faq>(this.apiUrl, faq);
+  }
+
+  updateFaq(id: number, faq: Faq): Observable<Faq> {
+    return this.http.put<Faq>(`${this.apiUrl}/${id}`, faq);
+  }
+
+  deleteFaq(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
