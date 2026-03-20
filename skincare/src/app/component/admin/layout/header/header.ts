f6c7bad3112d3750b7845606b9e359b8a30d0b46
patelmamta2309 <patelmamta.git@gmail.com>
2026-03-20 09:34:20 +0530

    header changes

--- skincare/src/app/component/admin/layout/header/header.ts
+++ skincare/src/app/component/admin/layout/header/header.ts
@@ -10,30 +10,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
-  user: any = { name: '', profile_image: '' };
+  public user: any = { name: '', profile_image: '' };
+  public displayName: string = '';
+  public userImagePath: string = 'assets/img/avatar/avatar-1.png';
 
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
     this.authService.currentUser$.subscribe(user => {
       this.user = user;
+      this.updateHeaderData();
       console.log('Header user state updated:', this.user);
     });
   }
 
-  get displayName(): string {
-    if (!this.user) return '';
-    return this.user.name || (this.user.first_name ? `${this.user.first_name} ${this.user.last_name || ''}` : '');
-  }
-
-  get userImagePath(): string {
-    if (this.user?.profile_image) {
-      return `http://127.0.0.1:8000/storage/${this.user.profile_image}`;
+  private updateHeaderData(): void {
+    if (!this.user) {
+      this.displayName = '';
+      this.userImagePath = 'assets/img/avatar/avatar-1.png';
+      return;
     }
-    return 'assets/img/avatar/avatar-1.png';
+
+    // Set displayName
+    this.displayName = this.user.name || (this.user.first_name ? `${this.user.first_name} ${this.user.last_name || ''}` : '');
+
+    // Set userImagePath
+    if (this.user.profile_image) {
+      this.userImagePath = `http://127.0.0.1:8000/storage/${this.user.profile_image}`;
+    } else {
+      this.userImagePath = 'assets/img/avatar/avatar-1.png';
+    }
   }
 
-  logout(): void {
+  public logout(): void {
     this.authService.logout();
     window.location.reload();
   }
